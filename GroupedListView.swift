e29f7bf8fed900b8fd60a8f94013c0412a99629c
Terence Ndabereye <ndabereye@gmail.com>
2024-07-06 13:38:51 +0300

    slightly changed accent color and added an app delegate to swiftui app. Will later add app rotation based on current view, so that previews can be rotated

---
+++ GroupedListView.swift
@@ -0,0 +1,18 @@
+//
+//  GroupedListView.swift
+//  PrimeFinderiOS
+//
+//  Created by Terence  Ndabereye  on 21/06/2024.
+//
+
+import SwiftUI
+
+struct GroupedListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroupedListView()
+}
---
+++ PrimeFinderiOS/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  PrimeFinderiOS
+//
+//  Created by Terence  Ndabereye  on 17/06/2024.
+//
+
+import Foundation
